2ef46ba5728473aa3a9f610a87eb1e0f33b48019
raigormax37417 <raigormax37417@gmail.com>
2021-12-11 22:23:59 -0600

    Cambio de Libreria,PDF en Ventas

--- venta/imprimir.php
+++ venta/imprimir.php
@@ -1,5 +1,6 @@
 <?php 
 ob_start();
+include "../session.php";
 include "../conexion.php";
 $con = conectar();  
     $id=$_GET['id'];
@@ -10,7 +11,7 @@ $con = conectar();
 
     $consulta="SELECT *  FROM garrafon";
     $qy=mysqli_query($con,$consulta);
-
+  
     $consulta2="SELECT *  FROM empleado";
     $qy2=mysqli_query($con,$consulta2);
 
@@ -32,15 +33,34 @@ $con = conectar();
             <div class="container mt-5">
                     <div class="row"> 
                         <div class="col-md-3">
-                            <h1>Ticket</h1>
+                          <div class="col-1" style="font-size: 3rem; margin-left: 30%;">
+                            Ticket
+                          </div>
                                 <form action="#">
-                    
+
+                                <label for="fecha">Fecha</label>    
                                 <input type="text" class="form-control mb-3" name="fecha" value="<?php echo $row['fecha']; ?>"> 
-                                <input type="number" class="form-control mb-3" min="0" name="cantidad" value="<?php echo $row['cantidad']; ?>">
-                                    <input  type="text" class="form-control mb-3" name="id_g" value="<?php echo $row['Id_Garrafon']; ?>">
-                                    <input  type="text" class="form-control mb-3" name="id_e" value="<?php echo $row['Id_Empleado']; ?>">
-                                    <input  type="text" class="form-control mb-3" name="id_c" value="<?php echo $row['Id_Cliente']; ?>">
-                                    
+
+                                <label for="cantidad">Cantidad</label>    
+                                <input type="text" class="form-control mb-3" min="0" name="cantidad" value="<?php echo $row['cantidad']; ?>">
+
+                                <?php 
+                                  while($rowG = mysqli_fetch_assoc($qy)){
+                                  if($row['Id_Garrafon'] == $rowG['Id_Garrafon']){  ?> 
+                                    <label for="id_g">Garrafon</label>    
+                                    <input  type="text" class="form-control mb-3" name="id_g" value="<?php echo "Garrafon ".$rowG['color']." Precio $".$rowG['precio_venta']; }} ?>">
+                                  <?php
+                                  while($rowE = mysqli_fetch_assoc($qy2)){
+                                    if($row['Id_Empleado'] == $rowE['Id_Empleado']){  ?> 
+                                    <label for="id_e">Empleado</label>    
+                                    <input  type="text" class="form-control mb-3" name="id_e" value="<?php echo $rowE['nombres']." ".$rowE['apellidos']; }}?>">
+
+                                  <?php
+                                  while($rowC = mysqli_fetch_assoc($qy3)){
+                                  if($row['Id_Cliente'] == $rowC['Id_Cliente']){  ?> 
+                                    <label for="id_c">Cliente</label>    
+                                    <input  type="text" class="form-control mb-3" name="id_c" value="<?php echo $rowC['nombres']." ".$rowC['apellidos']; }} ?>">
+                                    <label for="importe_total">Total</label>    
                                     <input type="text" class="form-control mb-3" name="importe_total" value="<?php echo $row['importe_total']; ?>">
                                 </form>
 
@@ -60,8 +80,8 @@ $dompdf = new Dompdf();
 $options = $dompdf->getOptions();
 $options->set(array('isRemoteEnabled' => true));
 $dompdf->setOptions($options);
-$dompdf->loadHtml("Contenido PDF");
+$dompdf->loadHtml($html);
 $dompdf->setPaper('letter');
 $dompdf->render();
-$dompdf->stream();
+$dompdf->stream("Ticket.pdf", array("Attachment" => false));
 ?>
